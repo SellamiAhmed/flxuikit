6b80a7f70d6fa8a5ef59ce028653732c50bca690
SellamiAhmed <ahmedsellami375@gmail.com>
2026-08-14 21:44:24 +0100

    fix: correct case-sensitive folder names and add missing className prop to PageShellBaseTitleProps

--- packages/uikit/src/business/PageShell/PageShellBaseTitle.tsx
+++ packages/uikit/src/business/PageShell/PageShellBaseTitle.tsx
@@ -4,15 +4,12 @@ import { Typography, TypographyProps } from '../../primitive/index.js'
 
 import classes from './PageShellBase.module.css'
 
-export interface PageShellBaseTitleProps extends React.PropsWithChildren<TypographyProps> {}
+export interface PageShellBaseTitleProps extends React.PropsWithChildren<TypographyProps> {
+  className?: string
+}
 
 export const PageShellBaseTitle = (props: PageShellBaseTitleProps) => {
   return (
-    <Typography
-      variant="headline-lg"
-      component="div"
-      {...props}
-      className={clsx(classes.title, props.className)}
-    />
+    <Typography variant="headline-lg" component="div" {...props} className={clsx(classes.title, props.className)} />
   )
 }
